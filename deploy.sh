59efd9d7322932eaa1be3737b1861c4554fcff2c
root <root@ip-172-31-35-210.ap-northeast-3.compute.internal>
2026-03-07 12:04:19 +0000

    final

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,39 @@ ACCOUNT_ID=860616632758
 
 docker login -u AWS -p $(aws ecr get-login-password --region $AWS_REGION) $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-docker pull $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/frontend-repo:latest
+docker network create app-network || true
+
+echo "Stopping old containers..."
+
+docker stop frontend backend mysql-db || true
+docker rm frontend backend mysql-db || true
+
+echo "Pulling images..."
+
+docker pull $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/database-repo:latest
 docker pull $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/backend-repo:latest
+docker pull $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/frontend-repo:latest
 
-docker stop frontend backend || true
-docker rm frontend backend || true
+echo "Starting database..."
 
-docker run -d --name backend --network app-network -p 8000:8000 $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/backend-repo:latest
-docker run -d --name frontend --network app-network -p 80:80 $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/frontend-repo:latest
+docker run -d \
+--name mysql-db \
+--network app-network \
+-p 3306:3306 \
+$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/database-repo:latest
+
+echo "Starting backend..."
+
+docker run -d \
+--name backend \
+--network app-network \
+-p 8000:8000 \
+$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/backend-repo:latest
+
+echo "Starting frontend..."
+
+docker run -d \
+--name frontend \
+--network app-network \
+-p 80:80 \
+$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/frontend-repo:latest
